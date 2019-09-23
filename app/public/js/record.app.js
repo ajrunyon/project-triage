b64319d997c6d999a037a497ffd02787cc930b0b
Andrew <ajrunyon@iu.edu>
2019-09-23 08:50:44 -0400

    stash

--- app/public/js/record.app.js
+++ app/public/js/record.app.js
@@ -1,16 +1,29 @@
 var patientRecordApp = new Vue({
   el: '#patientRecordApp',
   data: {
-    patients: []
+    patients: [],
+    formPatient:{
+      firstname:'',
+      lastname:'',
+      dob:'',
+      SAB:'',
+    },
   },
   methods: {
     fetchPatients() {
       fetch('dummy.php')
       .then(response => response.json())
       .then(json => { patientRecordApp.patients = json })
+    },
+    handleCreaterRecord(event) {
+      this.patients.push(this.formPatient);
+
+      this.formPatient = {
+          firstname:'',
+          lastname:'',
+          dob:'',
+          SAB:''
+        }
     }
-  },
-  created() {
-    this.fetchPatients();
   }
 });
